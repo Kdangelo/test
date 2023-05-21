fe057550ef90b9d8d89bb237b8e9b77f54651855
Kdangelo <kdangelof@gmail.com>
2023-05-21 14:39:17 -0400

    info api

--- src/MiApi.jsx
+++ src/MiApi.jsx
@@ -1,19 +1,37 @@
+import React from "react";
 import { useEffect, useState } from "react";
 
 const MiApi = () => {
   const [info, setInfo] = useState([]);
+
   useEffect(() => {
-    informationApi();
+    fetchData();
   }, []);
 
-  //consulta infromacion API
-  const informationApi = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/ditto";
-    const response = await fetch(url);
-    const data = await response.json();
-    setInfo(`${data}`);
+  //consulta informacion API
+  const fetchData = async () => {
+    try {
+      const url = "https://pokeapi.co/api/v2/pokemon/";
+      const response = await fetch(url);
+      const data = await response.json();
+      setInfo(data.results);
+      console.log(data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
   };
-  return <div>{info}</div>;
+  return (
+    <div>
+      {/* renderizo los datos recibidos de la API */}
+      {info && (
+        <ul>
+          {info.map((item) => (
+            <li key={item.name}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MiApi;
